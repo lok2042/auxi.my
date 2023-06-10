a4a9b53fdbc43230c55718552d9690a0992221a3
J Lok <73649534+lok2042@users.noreply.github.com>
2023-06-10 18:03:15 +0800

    Added smooth scrolling in script.js

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ submit.addEventListener("click", (e) => {
 
   window.open(`mailto:auxi.my001@gmail.com?subject=${subject}&body=${body}`);
 });
+
+// Smooth Scrolling
+document.querySelector(".main-nav").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("main-nav-link")) {
+    const id = e.target.getAttribute("href");
+
+    if (id.includes("pages/")) {
+      // Navigate to another screen
+      window.location.href = id;
+    } else {
+      // Scroll down to selected section
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    }
+  }
+});
